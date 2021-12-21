8ed0b139b6ae14333d6cd248b67c0566396c6caf
Papi <echucks19@gmail.com>
2021-12-21 21:06:23 +0000

    remove active link tracking

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,33 +1,21 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 import siteData from "../data/siteData";
 
 const navigation = siteData.navigation;
 
 function Navigation({ id }: NavigationProps) {
-  const router = useRouter();
-
   return (
     <nav id={id}>
       <ul className="inline-flex space-x-2">
-        {navigation.map((navItem, idx) => {
-          const href = idx ? `/#${navItem}` : "/";
-          const showActive = id == "main navigation" && router.asPath === href;
-
-          return (
-            <li key={navItem}>
-              <Link href={href}>
-                <a
-                  className={`capitalize hover:opacity-60 transition-all font-semibold py-1 px-2 ${
-                    showActive ? "underline" : ""
-                  }`}
-                >
-                  {navItem}
-                </a>
-              </Link>
-            </li>
-          );
-        })}
+        {navigation.map((navItem, idx) => (
+          <li key={navItem}>
+            <Link href={idx ? `/#${navItem}` : "/"}>
+              <a className="px-2 py-1 font-semibold capitalize transition-opacity hover:opacity-60">
+                {navItem}
+              </a>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
