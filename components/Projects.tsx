92704345f02e80e0cd18d75c5866f34f17f0efad
Papi <echucks19@gmail.com>
2021-12-21 21:00:39 +0000

    set projects scaffold

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,8 +1,17 @@
 function Projects() {
   return (
     <div id="projects">
-      <h2 className="text-3xl font-bold">Projects</h2>
-      <p>Comming soon...</p>
+      <h2 className="px-4 mx-auto text-3xl font-bold lg:max-w-4xl">Projects</h2>
+      <div className="flex my-16 space-x-4 overflow-x-auto scroll-mx-60">
+        {[0, 1, 2, 3, 4, 5].map((i) => (
+          <div
+            key={i}
+            className="flex items-center justify-center space-y-4 bg-gray-200 min-w-[20rem] h-96"
+          >
+            Project {i}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
