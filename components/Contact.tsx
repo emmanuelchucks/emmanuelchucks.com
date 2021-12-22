88548b6602ec16e859f231fb1e40cd7926c72741
Papi <echucks19@gmail.com>
2021-12-22 10:14:18 +0000

    feat(contact): form working with mock input
    - success alert
    - a11y improvements
    - mock async submit

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,13 +1,26 @@
-import { FieldError, Path, useForm, UseFormRegister } from "react-hook-form";
+import {
+  FieldError,
+  Path,
+  useForm,
+  UseFormRegister,
+  ValidationRule,
+} from "react-hook-form";
+import { FiCheckCircle } from "react-icons/fi";
 
 function Contact() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting, isSubmitSuccessful },
   } = useForm<FormData>();
 
-  const onSubmit = (data: FormData) => console.log(data);
+  const onSubmit = (data: FormData) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(console.log(data));
+      }, 2000);
+    });
+  };
 
   return (
     <div id="contact" className="px-4 mx-auto lg:max-w-4xl">
@@ -23,10 +36,12 @@ function Contact() {
           required
         />
         <InputField
+          type="email"
           label="email"
           placeholder="leonardo@email.com"
           register={register}
           error={errors.email}
+          pattern={/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i}
           required
         />
         <InputField
@@ -36,27 +51,44 @@ function Contact() {
           register={register}
           error={errors.message}
         />
-        <button
-          type="submit"
-          className="px-8 py-2 font-semibold bg-gray-900 rounded-md text-gray-50 hover:bg-gray-700"
-        >
-          Send
-        </button>
+
+        {isSubmitSuccessful ? (
+          <div
+            role="alert"
+            className="flex items-center gap-2 px-4 py-2 text-green-800 bg-green-100 md:max-w-[60%]"
+          >
+            <FiCheckCircle />
+            {"Email sent! I'll get back to you soon."}
+          </div>
+        ) : (
+          <button
+            type="submit"
+            aria-disabled={isSubmitting}
+            disabled={isSubmitting}
+            className="px-12 py-2 font-semibold transition-colors bg-gray-900 rounded-md text-gray-50 hover:opacity-80 disabled:opacity-40"
+          >
+            {isSubmitting ? "Sending..." : "Send"}
+          </button>
+        )}
       </form>
     </div>
   );
 }
 
 function InputField({
+  type,
   label,
   register,
   required,
   placeholder,
   isTextarea,
   error,
+  pattern,
 }: InputFieldProps) {
   const Component = isTextarea ? "textarea" : "input";
-  const errorMessage = error?.type === "required" && "This field is required";
+  const errorMessage =
+    (error?.type === "required" && "This field is required") ||
+    (error?.type === "pattern" && "Please enter a valid email address");
 
   return (
     <div className="block space-y-2">
@@ -68,16 +100,21 @@ function InputField({
       </label>
 
       <Component
-        {...register(label, { required })}
+        {...register(label, { required, pattern })}
+        id={label}
+        type={type}
         placeholder={placeholder}
-        className={`block w-full px-2 py-1 border-2 rounded-md md:w-1/2 ${
+        aria-invalid={error ? "true" : "false"}
+        className={`block w-full px-2 py-1 border-2 rounded-md md:w-[60%] ${
           errorMessage ? "outline-red-600 border-red-600" : ""
         } ${isTextarea ? "h-32" : ""}`}
       />
 
-      <p className={`text-sm ${errorMessage ? "text-red-600" : ""}`}>
-        {errorMessage}
-      </p>
+      {errorMessage && (
+        <span role="alert" className="text-sm text-red-600">
+          {errorMessage}
+        </span>
+      )}
     </div>
   );
 }
@@ -85,10 +122,12 @@ function InputField({
 type InputFieldProps = {
   register: UseFormRegister<FormData>;
   label: Path<FormData>;
+  type?: string;
   error?: FieldError;
   placeholder: string;
   isTextarea?: boolean;
-  required?: boolean;
+  pattern?: ValidationRule<RegExp>;
+  required?: ValidationRule<boolean>;
 };
 
 type FormData = {
