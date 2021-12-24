3c57abcddca33efdc662f80abe873471b82b51d8
Papi <echucks19@gmail.com>
2021-12-24 13:59:57 +0000

    hide scrollbars

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,11 +2,14 @@ function Projects() {
   return (
     <div id="projects">
       <h2 className="px-4 mx-auto text-3xl font-bold lg:max-w-4xl">Projects</h2>
-      <div className="flex my-16 space-x-4 overflow-x-auto snap-x scroll-pl-6">
+      <div
+        className="flex px-4 my-16 space-x-4 snap-x lg:px-96"
+        id="projectList"
+      >
         {[0, 1, 2, 3, 4, 5].map((i) => (
           <div
             key={i}
-            className="flex items-center justify-center space-y-4 bg-gray-200 min-w-[20rem] h-96 snap-start"
+            className="flex items-center justify-center bg-gray-200 min-w-[20rem] h-96 snap-center"
           >
             Project {i}
           </div>
