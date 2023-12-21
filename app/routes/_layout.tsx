91d71d2c3d2bf234259320278256dbcfd4850443
Emmanuel Chucks <echucks19@gmail.com>
2023-12-21 00:29:53 +0000

    feat(layout,index): copy over markup and styles
    - will enhance later
    - testing out sonik framework

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -7,16 +7,39 @@ const handler: LayoutHandler = async ({ children, head }) => (
 			<link href="/static/styles/global.css" rel="stylesheet" />
 			{head.createTags()}
 		</head>
-		<body>
-			<div class="wrapper">
-				<header>
-					<h1>
-						<a href="/">Top</a>
-					</h1>
+		<body class="grid bg-neutral-50 text-neutral-950 sm:grid-cols-[auto_60ch_auto] dark:bg-neutral-950 dark:text-neutral-50">
+			<div class="col-start-2 mx-4 my-16 grid gap-y-16">
+				<header class="text-neutral-600 dark:text-neutral-300">
+					<nav>
+						<ul>
+							<li>
+								<a href="/" class="underline underline-offset-4">
+									Home
+								</a>
+							</li>
+						</ul>
+					</nav>
 				</header>
 				{children}
-				<footer style="margin-top:2rem">
-					<small>© 2023 your name</small>
+				<footer class="flex gap-x-8 text-neutral-900 dark:text-neutral-300">
+					<a
+						href="https://github.com/emmanuelchucks"
+						class="underline underline-offset-4"
+					>
+						GitHub
+					</a>
+					<a
+						href="https://twitter.com/emmanuelchucks"
+						class="underline underline-offset-4"
+					>
+						Twitter
+					</a>
+					<a
+						href="https://buymeacoffee.com/emmanuelchucks"
+						class="underline underline-offset-4"
+					>
+						Buy me a coffee ☕️
+					</a>
 				</footer>
 			</div>
 		</body>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,11 +2,52 @@ import type { Context } from "sonik"
 
 export default async function Index(c: Context) {
 	return c.render(
-		<div>
-			<h2>
-				Hello <a href="/about/me">me</a>!
-			</h2>
-		</div>,
+		<main>
+			<article class="grid gap-y-4 leading-7">
+				<h1 class="sr-only">Full-Stack Software Engineer</h1>
+				<a
+					href="mailto:hi@emmanuelchucks.com?subject=Mail from Website"
+					class="rounded-lg border border-neutral-200 bg-neutral-100 px-4 py-2 text-center font-medium text-neutral-600 dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-300"
+				>
+					<span class="sr-only">Send me an email: </span>
+					hi@emmanuelchucks.com
+				</a>
+				<p>
+					In the digital realm, a coder emerges as a modern artisan, traversing
+					the realms of front-end and back-end with the grace of a seasoned
+					knight.
+				</p>
+				<p>
+					Armed with obnoxious TypeScript skills, he weaves intricate digital
+					tapestries that captivate all who behold them, employing algorithms as
+					finely tuned as celestial lyres.
+				</p>
+				<p>
+					A maestro of version control, he wields Git with the finesse of
+					Solomon's wisdom, ensuring no pull request goes astray in the bustling
+					land of startupshire.
+				</p>
+				<p>
+					His code commits, adorned with memes and punctuated with puns,
+					transform even mundane stand-up meetings into comedic galas.
+				</p>
+				<p>
+					Facing bugs and errors with a hearty laugh, he knows each fix is a
+					step closer to the digital nirvana sought by kings and queens of the
+					industry.
+				</p>
+				<p>
+					In the marketplace of ideas, he is a savvy trader, seamlessly
+					translating the desires of stakeholders into code that dances like
+					David before the Ark.
+				</p>
+				<p>
+					His name echoes through the annals of tech history as a beacon of both
+					humor and skill, a full-stack engineer who, like a digital troubadour,
+					brings laughter and functionality to the kingdom of Silicon.
+				</p>
+			</article>
+		</main>,
 		{
 			title: "Welcome to Sonik!",
 			meta: [{ name: "description", content: "This an example for Sonik" }],
