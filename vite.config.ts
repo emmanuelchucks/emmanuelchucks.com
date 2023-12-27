6d93ee72bc676feae39dce1edc2352609510f899
Emmanuel Chucks <echucks19@gmail.com>
2023-12-27 23:25:24 +0000

    BREAKING CHANGE: replace with solid start tailwind
    - sonik was not loading assets on cloudflare
    - will revisit later when it works

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,3 @@
-import pages from "@sonikjs/cloudflare-pages"
-import sonik from "sonik/vite"
-import { defineConfig } from "vite"
+import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({
-	plugins: [sonik(), pages()],
-})
+export default defineConfig({});
