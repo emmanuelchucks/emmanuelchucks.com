9e0f63954e465d68bc9ac9e08b6bce141255f476
Emmanuel Chucks <echucks19@gmail.com>
2023-12-28 01:58:43 +0000

    feat(index): first draft of new design and layout

--- src/app.tsx
+++ src/app.tsx
@@ -3,13 +3,16 @@ import { Router } from "@solidjs/router"
 import { FileRoutes } from "@solidjs/start"
 import { Suspense } from "solid-js"
 import "./app.css"
+import { RootLayout } from "./components/root-layout"
 
 export default function App() {
 	return (
 		<Router
 			root={(props) => (
 				<>
-					<Suspense>{props.children}</Suspense>
+					<RootLayout>
+						<Suspense>{props.children}</Suspense>
+					</RootLayout>
 				</>
 			)}
 		>
