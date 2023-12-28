f7fc0d35eaf3c44d1c879ec54e325760c55411bf
Emmanuel Chucks <echucks19@gmail.com>
2023-12-28 16:01:32 +0000

    refactor(app): rename import styles

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { Router } from "@solidjs/router"
 import { FileRoutes } from "@solidjs/start"
 import { Suspense } from "solid-js"
 import "./app.css"
-import { RootLayout } from "./components/root-layout"
+import { RootLayout } from "~/components/root-layout"
 
 export default function App() {
 	return (
