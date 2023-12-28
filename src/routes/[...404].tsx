aeb8d2c968d52d3eceb71afe5cfb3de04fe49cce
Emmanuel Chucks <echucks19@gmail.com>
2023-12-28 16:33:13 +0000

    feat(404): add a little pizzazz

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -2,9 +2,10 @@ import { HttpStatusCode } from "@solidjs/start"
 
 export default function NotFound() {
 	return (
-		<main>
+		<main class="grid place-content-center gap-y-2 text-center">
 			<HttpStatusCode code={404} />
-			<p>Not found</p>
+			<p class="text-8xl">𝟜⓪𝟺</p>
+			<h1>Page not found</h1>
 		</main>
 	)
 }
