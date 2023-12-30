61e57f3914ab102f41e156dea5975928bde98e7e
Emmanuel Chucks <echucks19@gmail.com>
2023-12-30 15:17:22 +0000

    feat(blog): test and validate mdx use

---
+++ src/components/counter.tsx
@@ -0,0 +1,13 @@
+import { createSignal } from "solid-js"
+
+export function Counter() {
+	const [count, setCount] = createSignal(0)
+	const increment = () => setCount(count() + 1)
+
+	return (
+		<div>
+			<p>Count: {count()}</p>
+			<button onClick={increment}>Increment</button>
+		</div>
+	)
+}
---
+++ src/routes/hello.tsx
@@ -0,0 +1,9 @@
+import Content from "../content/hello.mdx"
+
+export default function Hello() {
+	return (
+		<div class="prose dark:prose-invert">
+			<Content />
+		</div>
+	)
+}
