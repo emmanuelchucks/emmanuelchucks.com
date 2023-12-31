604f4a090566aa6283e2f476a0946b537f7be5cd
Emmanuel Chucks <echucks19@gmail.com>
2023-12-31 17:52:30 +0000

    feat(blog): handle listing all posts and each post

--- src/routes/(root)/blog.tsx
+++
@@ -1,9 +0,0 @@
-import Content from "~/content/hello.mdx"
-
-export default function Blog() {
-	return (
-		<div class="prose dark:prose-invert prose-pre:bg-neutral-100 dark:prose-pre:bg-neutral-900">
-			<Content />
-		</div>
-	)
-}
