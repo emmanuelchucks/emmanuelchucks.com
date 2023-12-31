1ad81d9238753cfaa22f4266a294553bef3508ea
Emmanuel Chucks <echucks19@gmail.com>
2023-12-31 17:56:46 +0000

    refactor(blog): remove console log

--- src/routes/(root)/blog/index.tsx
+++ src/routes/(root)/blog/index.tsx
@@ -3,7 +3,6 @@ import { getAllPosts } from "~/lib/posts"
 
 export default function Blog() {
 	const posts = getAllPosts()
-	console.log(posts)
 
 	return (
 		<div class="prose dark:prose-invert prose-pre:bg-neutral-100 dark:prose-pre:bg-neutral-900">
