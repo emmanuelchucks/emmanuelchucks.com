972e544a79eeb17aa011f0f2406ac7b8803522ff
Emmanuel Chucks <echucks19@gmail.com>
2023-12-31 16:17:52 +0000

    feat(blog): add blog page with nav link

--- src/components/root-layout.tsx
+++ src/components/root-layout.tsx
@@ -2,19 +2,26 @@ import { type JSX } from "solid-js"
 
 export function RootLayout(props: { children: JSX.Element }) {
 	return (
-		<div class="mx-4 my-16 grid max-w-screen-sm gap-y-16 sm:mx-8 md:mx-auto">
-			<header class="font-semibold text-neutral-800 dark:text-neutral-300">
-				<nav>
-					<ul>
-						<li>
-							<a href="/" class="underline-offset-4 hover:underline">
-								Home
-							</a>
-						</li>
-					</ul>
-				</nav>
-			</header>
-			{props.children}
+		<div class="mx-4 my-16 grid min-h-screen items-start sm:mx-8 md:mx-auto md:grid-cols-[auto_65ch_auto]">
+			<div class="grid gap-y-16 md:col-start-2">
+				<header class="font-semibold text-neutral-800 dark:text-neutral-300">
+					<nav>
+						<ul class="flex flex-row gap-x-8">
+							<li>
+								<a href="/" class="underline-offset-4 hover:underline">
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="/blog" class="underline-offset-4 hover:underline">
+									Blog
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</header>
+				{props.children}
+			</div>
 		</div>
 	)
 }
