e95c32a107d2bde31984e5ff32fc6bca6fa188a9
Emmanuel Chucks <echucks19@gmail.com>
2023-12-31 20:18:34 +0000

    fix(blog): prevent code block from overflowing

--- src/routes/(root)/blog/[id].tsx
+++ src/routes/(root)/blog/[id].tsx
@@ -6,7 +6,7 @@ export default function Post() {
 	const { default: Content } = getPostById(parmas.id)
 
 	return (
-		<div class="prose prose-neutral dark:prose-invert prose-pre:bg-neutral-100 dark:prose-pre:bg-neutral-900">
+		<div class="prose prose-neutral min-w-0 dark:prose-invert prose-pre:bg-neutral-100 dark:prose-pre:bg-neutral-900">
 			<Content />
 		</div>
 	)
