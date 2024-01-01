4f1bf5ee3044438ab5ed78e908c18e16577cd93b
Emmanuel Chucks <echucks19@gmail.com>
2024-01-01 12:49:45 +0000

    feat(blog): enable filtering posts by query
    - works with or without JavaScript enabled

--- src/routes/(root)/blog/index.tsx
+++ src/routes/(root)/blog/index.tsx
@@ -1,12 +1,40 @@
+import {
+	createAsync,
+	useSearchParams,
+	type RouteDefinition,
+} from "@solidjs/router"
 import { For } from "solid-js"
-import { getAllPosts } from "~/lib/posts"
+import { getPostsByQuery, handleSearchPosts } from "~/lib/posts"
+
+export const route = {
+	load({ params }) {
+		void getPostsByQuery(params.q)
+	},
+} satisfies RouteDefinition
 
 export default function Blog() {
-	const posts = getAllPosts()
+	const [searchParams, setSearchParams] = useSearchParams()
+	const posts = createAsync(async () => getPostsByQuery(searchParams.q))
 
 	return (
-		<For each={posts}>
-			{(post) => <a href={post.frontmatter.slug}>{post.frontmatter.title}</a>}
-		</For>
+		<div class="grid gap-y-16">
+			<form action={handleSearchPosts} method="post">
+				<label for="q" class="sr-only">
+					Search
+				</label>
+				<input
+					id="q"
+					name="q"
+					type="search"
+					onInput={(e) => {
+						setSearchParams({ q: e.currentTarget.value })
+					}}
+					class="w-full rounded-md bg-neutral-100 px-2 py-1 text-lg dark:bg-neutral-900"
+				/>
+			</form>
+			<For each={posts()}>
+				{(post) => <a href={post.frontmatter.slug}>{post.frontmatter.title}</a>}
+			</For>
+		</div>
 	)
 }
