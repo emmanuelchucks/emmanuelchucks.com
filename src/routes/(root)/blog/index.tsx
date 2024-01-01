32f44d238d3b2de539a709f7555a1dc4f0e36d29
Emmanuel Chucks <echucks19@gmail.com>
2024-01-01 01:53:08 +0000

    refactor(blog): remove unused wrapper div

--- src/routes/(root)/blog/index.tsx
+++ src/routes/(root)/blog/index.tsx
@@ -5,10 +5,8 @@ export default function Blog() {
 	const posts = getAllPosts()
 
 	return (
-		<div class="prose dark:prose-invert prose-pre:bg-neutral-100 dark:prose-pre:bg-neutral-900">
-			<For each={posts}>
-				{(post) => <a href={post.frontmatter.slug}>{post.frontmatter.title}</a>}
-			</For>
-		</div>
+		<For each={posts}>
+			{(post) => <a href={post.frontmatter.slug}>{post.frontmatter.title}</a>}
+		</For>
 	)
 }
