3df79a4bba1057f8d530aa6625757c0983a3e4da
Emmanuel Chucks <echucks19@gmail.com>
2024-01-01 13:35:27 +0000

    feat(blog): enhance styling and a11y of search

--- src/routes/(root)/blog/index.tsx
+++ src/routes/(root)/blog/index.tsx
@@ -17,20 +17,30 @@ export default function Blog() {
 	const posts = createAsync(async () => getPostsByQuery(searchParams.q))
 
 	return (
-		<div class="grid gap-y-16">
+		<div class="grid gap-y-10">
 			<form action={handleSearchPosts} method="post">
-				<label for="q" class="sr-only">
+				<label for="query" class="sr-only">
 					Search
 				</label>
 				<input
-					id="q"
 					name="q"
+					id="query"
 					type="search"
+					placeholder="Search posts"
 					onInput={(e) => {
 						setSearchParams({ q: e.currentTarget.value })
 					}}
-					class="w-full rounded-md bg-neutral-100 px-2 py-1 text-lg dark:bg-neutral-900"
+					class="w-full rounded-md bg-neutral-100 px-4 py-2 placeholder:text-neutral-400 dark:bg-neutral-900 dark:placeholder:text-neutral-600"
 				/>
+				<noscript>
+					<p class="mt-2 text-sm text-neutral-500">
+						Press
+						<kbd class="mx-1 rounded bg-neutral-100 px-2 py-1 dark:bg-neutral-900">
+							Enter
+						</kbd>
+						to search
+					</p>
+				</noscript>
 			</form>
 			<For each={posts()}>
 				{(post) => <a href={post.frontmatter.slug}>{post.frontmatter.title}</a>}
