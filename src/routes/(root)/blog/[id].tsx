9d9a3fc7d2fe8694edf568ceebd5f4abb5b1aaf1
Emmanuel Chucks <echucks19@gmail.com>
2024-01-02 22:42:31 +0000

    feat(meta): use solid meta for title and description

--- src/routes/(root)/blog/[id].tsx
+++ src/routes/(root)/blog/[id].tsx
@@ -1,3 +1,4 @@
+import { Meta, Title } from "@solidjs/meta"
 import { useParams } from "@solidjs/router"
 import { formatDate, getPostById } from "~/lib/posts"
 
@@ -6,16 +7,20 @@ export default function Post() {
 	const { default: Content, frontmatter } = getPostById(parmas.id)
 
 	return (
-		<main class="grid gap-y-4">
-			<time
-				dateTime={frontmatter.publishedAt}
-				class="text-sm text-neutral-600 dark:text-neutral-400"
-			>
-				{formatDate(frontmatter.publishedAt, "long")}
-			</time>
-			<article class="prose prose-neutral min-w-0 dark:prose-invert prose-pre:bg-neutral-100 dark:prose-pre:bg-neutral-900 dark:[&_.shiki_span]:!text-[--shiki-dark]">
-				<Content />
-			</article>
-		</main>
+		<>
+			<Title>{frontmatter.title} - Emmanuel Chucks</Title>
+			<Meta name="description" content={frontmatter.description} />
+			<main class="grid gap-y-4">
+				<time
+					dateTime={frontmatter.publishedAt}
+					class="text-sm text-neutral-600 dark:text-neutral-400"
+				>
+					{formatDate(frontmatter.publishedAt, "long")}
+				</time>
+				<article class="prose prose-neutral min-w-0 dark:prose-invert prose-pre:bg-neutral-100 dark:prose-pre:bg-neutral-900 dark:[&_.shiki_span]:!text-[--shiki-dark]">
+					<Content />
+				</article>
+			</main>
+		</>
 	)
 }
