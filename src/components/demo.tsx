e4e1e5033560282ee69c9971182af2c38f5ea5c5
Emmanuel Chucks <echucks19@gmail.com>
2024-01-03 01:24:24 +0000

    fix(post): make scrollable for keyboard users

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -1,16 +1,23 @@
-import { createUniqueId, type JSX } from "solid-js"
+import { createUniqueId, splitProps, type JSX } from "solid-js"
 
-export function Demo(props: { heading: string; children: JSX.Element }) {
+export function Demo(props: DemoProps) {
 	const id = createUniqueId()
+	const [local, rest] = splitProps(props, ["heading", "children"])
 	return (
 		<section
 			aria-labelledby={id}
 			class="max-h-96 min-h-72 overflow-y-auto rounded-md bg-neutral-100 px-8 py-2 sm:px-24 sm:py-8 dark:bg-neutral-900"
+			{...rest}
 		>
 			<h3 id={id} class="sr-only">
-				Demo for {props.heading}
+				Demo for {local.heading}
 			</h3>
-			{props.children}
+			{local.children}
 		</section>
 	)
+}
+
+type DemoProps = JSX.IntrinsicElements["section"] & {
+	heading: string
+	children: JSX.Element
 }
