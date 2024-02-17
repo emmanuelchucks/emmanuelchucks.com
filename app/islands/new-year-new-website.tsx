d5e44c58c830603751378e3890f8cac54f02988a
Emmanuel Chucks <echucks19@gmail.com>
2024-02-17 21:32:42 +0000

    style(tailwind): use cx for organization

--- app/islands/new-year-new-website.tsx
+++ app/islands/new-year-new-website.tsx
@@ -1,3 +1,4 @@
+import { cx } from "hono/css"
 import { useState } from "hono/jsx"
 
 const names = ["The Primeagen", "TJ DeVries", "Yusuke Wada"]
@@ -31,12 +32,24 @@ export async function StyledSearchInput() {
 					onInput={(event) => {
 						setSearchQuery((event.currentTarget as HTMLInputElement).value)
 					}}
-					class="w-full rounded-md bg-neutral-50 px-4 py-2 placeholder:text-neutral-400 dark:bg-neutral-800 dark:placeholder:text-neutral-600 [&::-webkit-search-cancel-button]:ms-4"
+					class={cx(
+						"w-full rounded-md bg-neutral-50 px-4 py-2",
+						"placeholder:text-neutral-400 placeholder:dark:text-neutral-600",
+						"[&::-webkit-search-cancel-button]:ms-4",
+						"dark:bg-neutral-800",
+					)}
 				/>
 				<noscript>
-					<p class="prose prose-neutral dark:prose-invert mt-2 text-sm text-neutral-600 dark:text-neutral-400">
+					<p
+						class={cx(
+							"prose prose-neutral mt-2 text-sm text-neutral-600",
+							"dark:prose-invert dark:text-neutral-400",
+						)}
+					>
 						Press
-						<kbd class="mx-1 bg-neutral-100 dark:bg-neutral-900">Enter</kbd>
+						<kbd class={cx("mx-1 bg-neutral-100", "dark:bg-neutral-900")}>
+							Enter
+						</kbd>
 						to search
 					</p>
 				</noscript>
