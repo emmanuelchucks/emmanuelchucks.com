a070f048b08c5c7038d7fee31505954d6a6cc481
Emmanuel Chucks <echucks19@gmail.com>
2024-02-17 21:32:42 +0000

    style(tailwind): use cx for organization

--- components/demo-box.tsx
+++ components/demo-box.tsx
@@ -1,3 +1,5 @@
+import { cx } from "hono/css"
+
 export async function DemoBox(props: {
 	heading: string
 	children: JSX.Element
@@ -6,7 +8,11 @@ export async function DemoBox(props: {
 	return (
 		<section
 			aria-labelledby={id}
-			class="max-h-96 min-h-72 overflow-y-auto rounded-md bg-white px-8 py-2 sm:px-24 sm:py-8 dark:bg-neutral-900"
+			class={cx(
+				"max-h-96 min-h-72 overflow-y-auto rounded-md bg-white px-8 py-2",
+				"dark:bg-neutral-900",
+				"sm:px-24 sm:py-8",
+			)}
 			tabindex={0}
 		>
 			<h3 id={id} class="sr-only">
