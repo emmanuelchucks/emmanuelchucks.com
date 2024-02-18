f48cdc86a52e55fb6bc20ce169ca48037f77afae
Emmanuel Chucks <echucks19@gmail.com>
2024-02-18 13:26:06 +0000

    style(wrong-correct): reduce heading font size

--- components/wrong-correct.tsx
+++ components/wrong-correct.tsx
@@ -2,7 +2,7 @@ export async function Wrong(props: { heading: string; children: JSX.Element }) {
 	const id = "wrong-" + Math.random().toString().substring(2)
 	return (
 		<section aria-labelledby={id}>
-			<h3 id={id}>
+			<h3 id={id} class="text-base font-medium">
 				<span aria-hidden="true">❌</span> Wrong
 				<span class="sr-only"> version for {props.heading}</span>
 			</h3>
@@ -18,7 +18,7 @@ export async function Correct(props: {
 	const id = "correct-" + Math.random().toString().substring(2)
 	return (
 		<section aria-labelledby={id}>
-			<h3 id={id}>
+			<h3 id={id} class="text-base font-medium">
 				<span aria-hidden="true">✅</span> Correct
 				<span class="sr-only"> version for {props.heading}</span>
 			</h3>
