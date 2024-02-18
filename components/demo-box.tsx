1714543c6e4fe1e18c1a899a23082d14ba162d12
Emmanuel Chucks <echucks19@gmail.com>
2024-02-18 22:13:58 +0000

    fix(a11y): remove overuse of aria attributes

--- components/demo-box.tsx
+++ components/demo-box.tsx
@@ -4,10 +4,8 @@ export async function DemoBox(props: {
 	heading: string
 	children: JSX.Element
 }) {
-	const id = "demo-" + Math.random().toString().substring(2)
 	return (
 		<section
-			aria-labelledby={id}
 			class={cx(
 				"max-h-96 min-h-72 overflow-y-auto rounded-md bg-white px-8 py-2 outline-none ring-neutral-700",
 				"dark:bg-neutral-900 dark:ring-neutral-300",
@@ -16,9 +14,7 @@ export async function DemoBox(props: {
 			)}
 			tabindex={0}
 		>
-			<h3 id={id} class="sr-only">
-				Demo for {props.heading}
-			</h3>
+			<h3 class="sr-only">Demo for {props.heading}</h3>
 			{props.children}
 		</section>
 	)
--- components/wrong-correct.tsx
+++ components/wrong-correct.tsx
@@ -1,8 +1,7 @@
 export async function Wrong(props: { heading: string; children: JSX.Element }) {
-	const id = "wrong-" + Math.random().toString().substring(2)
 	return (
-		<section aria-labelledby={id}>
-			<h3 id={id} class="text-base font-medium">
+		<section>
+			<h3 class="text-base font-medium">
 				<span aria-hidden="true">❌</span> Wrong
 				<span class="sr-only"> version for {props.heading}</span>
 			</h3>
@@ -15,10 +14,9 @@ export async function Correct(props: {
 	heading: string
 	children: JSX.Element
 }) {
-	const id = "correct-" + Math.random().toString().substring(2)
 	return (
-		<section aria-labelledby={id}>
-			<h3 id={id} class="text-base font-medium">
+		<section>
+			<h3 class="text-base font-medium">
 				<span aria-hidden="true">✅</span> Correct
 				<span class="sr-only"> version for {props.heading}</span>
 			</h3>
