dce6b059f8bbe864b489a0954a9b109443c53d41
Emmanuel Chucks <echucks19@gmail.com>
2024-02-18 22:13:58 +0000

    fix(a11y): remove overuse of aria attributes

--- app/islands/new-year-new-website.tsx
+++ app/islands/new-year-new-website.tsx
@@ -3,10 +3,6 @@ import { SearchInput } from "../../components/search-input"
 
 const names = ["The Primeagen", "TJ DeVries", "Yusuke Wada"]
 
-const searchQueryId = "search-query-" + Math.random().toString().substring(2)
-const searchResultsId =
-	"search-results-" + Math.random().toString().substring(2)
-
 export async function StyledSearchInput() {
 	const [searchQuery, setSearchQuery] = useState("")
 	const filteredNames = names.filter((name) =>
@@ -16,14 +12,14 @@ export async function StyledSearchInput() {
 	return (
 		<div class="grid h-72 content-center">
 			<form
-				aria-labelledby={searchQueryId}
 				onSubmit={(e) => {
 					e.preventDefault()
 				}}
 			>
+				<legend class="sr-only">Search names of prolific programmers</legend>
 				<SearchInput
+					id="search-names"
 					name="q"
-					id={searchQueryId}
 					type="search"
 					placeholder="Search names"
 					onInput={(e) => {
@@ -31,12 +27,8 @@ export async function StyledSearchInput() {
 					}}
 				/>
 			</form>
-			<section
-				aria-live="polite"
-				aria-labelledby={searchResultsId}
-				class="min-h-32"
-			>
-				<h3 id={searchResultsId} class="sr-only">
+			<section aria-live="polite" class="min-h-32">
+				<h3 class="sr-only">
 					{searchQuery ? `Results for ${searchQuery}` : "All names"}
 				</h3>
 				{filteredNames.length ? (
