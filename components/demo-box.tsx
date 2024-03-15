734117ec87819a240041b7528578ff1c2f2d91d5
Emmanuel Chucks <echucks19@gmail.com>
2024-03-15 01:01:39 +0000

    feat(new-year-new-website): add toggle to dark mode demo

--- components/demo-box.tsx
+++ components/demo-box.tsx
@@ -1,18 +1,11 @@
 import { cx } from "hono/css"
+import { type Child } from "hono/jsx"
 
-export async function DemoBox(props: {
-	heading: string
-	children: JSX.Element
-}) {
+/* eslint-disable-next-line @typescript-eslint/promise-function-async */
+export function DemoBox(props: { heading: string; children: Child }) {
 	return (
 		<section
-			class={cx(
-				"max-h-96 min-h-72 overflow-y-auto rounded-md bg-white px-8 py-2 outline-none ring-neutral-700",
-				"dark:bg-neutral-900 dark:ring-neutral-300",
-				"focus-visible:ring-2",
-				"sm:px-24 sm:py-8",
-			)}
-			tabindex={0}
+			class={cx("overflow-clip rounded-md bg-white", "dark:bg-neutral-900")}
 		>
 			<h3 class="sr-only">Demo for {props.heading}</h3>
 			{props.children}
