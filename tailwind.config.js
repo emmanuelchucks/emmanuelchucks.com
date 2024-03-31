d03b46de830e4da7322f83ee8166a76bee84d8f5
Emmanuel Chucks <echucks19@gmail.com>
2024-03-31 23:16:44 +0000

    chore(tailwind-config): use import for typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import typography from "@tailwindcss/typography"
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["app/**/*.tsx"],
 	theme: {
 		extend: {},
 	},
-	plugins: [require("@tailwindcss/typography")],
+	plugins: [typography],
 }
