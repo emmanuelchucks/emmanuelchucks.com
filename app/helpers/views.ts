9eb69c46db1212b6fd531bee6586e67d7db9cee1
Emmanuel Chucks <echucks19@gmail.com>
2025-05-04 14:33:48 +0000

    feat(wrangler): add kv store for post views

--- app/helpers/views.ts
+++ app/helpers/views.ts
@@ -1,8 +1,11 @@
-import type { Env } from "hono";
 import { getContext } from "hono/context-storage";
 
+interface HonoEnv {
+	Bindings: Env;
+}
+
 export async function getFormattedViewsCount(id: string): Promise<string> {
-	const c = getContext<Env>();
+	const c = getContext<HonoEnv>();
 	const viewsCount = Number((await c.env.VIEWS_COUNTER.get(id)) ?? 2);
 	return new Intl.NumberFormat().format(viewsCount);
 }
