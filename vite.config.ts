fae28f7631eff2eb4615cca9f4def0f4eb9ff7bb
Emmanuel Chucks <echucks19@gmail.com>
2025-09-22 01:17:03 +0000

    chore(config): update lint, type, and build configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import contentCollections from "@content-collections/vite";
 import mdx from "@mdx-js/rollup";
 import rehypeShiki from "@shikijs/rehype";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { redwood } from "rwsdk/vite";
@@ -10,14 +11,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    redwood(),
-    tailwindcss(),
-    contentCollections(),
-    cloudflare({
-      viteEnvironment: {
-        name: "worker",
-      },
-    }),
     mdx({
       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
       rehypePlugins: [
@@ -32,5 +25,18 @@ export default defineConfig({
         ],
       ],
     }),
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    cloudflare({
+      viteEnvironment: {
+        name: "worker",
+      },
+    }),
+    redwood(),
+    tailwindcss(),
+    contentCollections(),
   ],
 });
