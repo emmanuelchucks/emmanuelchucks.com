05e3b1019bba7761485dd25a1bcdf0ab161ec18a
Emmanuel Chucks <echucks19@gmail.com>
2025-10-17 01:02:43 +0000

    style(egg): simplify console log styling

--- src/egg.ts
+++ src/egg.ts
@@ -1,8 +1,4 @@
-const prefersDark = globalThis.matchMedia(
-  "(prefers-color-scheme: dark)",
-).matches;
-const color = prefersDark ? "white" : "black";
 console.log(
-  "%c" + atob("ZW1tYW51ZWxjaHVja3MuY29t"),
-  `color:${color};font-size:16px;font-weight:900;text-shadow:2px 2px 5px rgba(0,0,0,0.1);`,
+  `%c${atob("ZW1tYW51ZWxjaHVja3MuY29t")}`,
+  `font-size:16px;font-weight:900;`,
 );
